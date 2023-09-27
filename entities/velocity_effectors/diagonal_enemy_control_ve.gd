3722cbaaf7566cd8522185ff3351363e7a36a9bd
eiparfenov <epar2002@outlook.com>
2023-09-27 17:21:56 +0300

    Minor change

--- entities/velocity_effectors/diagonal_enemy_control_ve.gd
+++ entities/velocity_effectors/diagonal_enemy_control_ve.gd
@@ -1,1 +1,23 @@
 extends BaseVelocityEffector
+
+var direction: Vector2
+
+
+const POSSIBLE_DIRECTIONS = [
+	Vector2(1, 1),
+	Vector2(1, -1),
+	Vector2(-1, 1),
+	Vector2(-1, -1),
+]
+
+
+func _ready():
+	direction = POSSIBLE_DIRECTIONS.pick_random()
+
+
+func effect_velocity(velocity: Vector2) -> Vector2:
+	return direction.normalized()
+
+
+func on_entity_collision(collision: KinematicCollision2D):
+	pass
